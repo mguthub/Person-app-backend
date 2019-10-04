a3391d907d44c541108d0b1570ea6e6af84e8cfb
Mayur <mayur.malhotra@gmail.com>
2019-10-03 23:34:58 -0400

    Added responseEntity to show a different way
    Added responseEntity to show a different way
    Changed port number in application.properties to 5000 for AWS hosting and use.

--- src/main/java/com/example/person/controller/PersonController.java
+++ src/main/java/com/example/person/controller/PersonController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,10 +33,13 @@ public class PersonController {
 		response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
 	}
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Person> getPersonByFirstName(@RequestParam("firstName") String firstName){
+	public ResponseEntity <List<Person>> getPersonByFirstName(@RequestParam("firstName") String firstName){
 		LOG.info("Firstname of person to search for is {}",firstName);
 		if(Optional.ofNullable(firstName).isPresent())
-			return personService.getPersonByFirstName(firstName);
+			return new ResponseEntity<>(
+					personService.getPersonByFirstName(firstName), 
+				      HttpStatus.OK);
+			//return personService.getPersonByFirstName(firstName);
 		return null;
 	}
 	
